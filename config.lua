c665fbd8c5b7bf1ec54257837d7f3bd16d592c9b
HXVRMXN-dev <107282158+HXVRMXN-dev@users.noreply.github.com>
2022-08-31 13:51:47 +0100

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,30 @@
+Configuration = {}
+
+-- Command
+Configuration.Command = "axon" -- Command which activates bodycam. Only put alphabetical letters in or numbers, no /, @'s ect because I havent fully tested such thing.
+Configuration.CommandSuggest = "/axon" -- Put command above after / or keep blank if you dont want any message to pop up when doing the command
+Configuration.MessageSuggest = "Toggle the setup for the AXON Bodycam 2021 Model" -- Put message what comes in the command suggestion it will show like CommandSuggestion MessageSuggestion, example: "Toggle the setup for the AXON Bodycam 2021 Model"
+
+-- Camera Modes
+Configuration.FirstPerson = true -- Turn to false if you want people to not be able to activate their bodycam in First Person
+Configuration.ThirdPersonClose = true -- Turn to false if you want people to not be able to activate their bodycam in Close Third Person
+Configuration.ThirdPersonMid = true -- Turn to false if you want people to not be able to activate their bodycam in Medium Third Person
+Configuration.ThirdPersonFar = true -- Turn to false if you want people to not be able to activate their bodycam in Far Third Person
+Configuration.ThirdPersonCinematic = true -- Turn to false if you want people to not be able to activate their bodycam in Cinematic Third Person
+
+-- Notify type
+Configuration.Notify = "None" -- Check below for what things to put in "" for your type of notification
+Configuration.Time = 6000 -- Time for Notification to display (OkOk Notify, if using do it in mil seconds e.g. 5000 = 5 Seconds, 3000 = 3 Seconds ect)
+
+--[[
+
+OkOk Notify - "OkOk"
+
+Mythic Notify - "Mythic"
+
+K5 Notify - "K5"
+
+Infinity Notify - "Infinity"
+
+Nothing - "None"
+]]
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+author 'HXVRMXN.dev`s Bodycam'
+description 'Perfect bodycam script which is edited to work in all viewmodes with more accessibility - github.com/HXVRMXN-dev/fivem-scripts-v1 for more scripts'
+fx_version 'adamant'
+game 'gta5'
+
+shared_script 'config.lua'
+client_script 'client.lua'
+server_script 'server.lua'
+
+
+ui_page 'RCPMenu.html'
+
+file 'RCPMenu.html'
